5adfcc6df6133efd79c1953f6f7ca92dc4a41caa
ypmen <ypmen@mpifr-bonn.mpg.de>
2024-04-04 21:28:25 +0200

    add construct function with json

--- include/equalize.h
+++ include/equalize.h
@@ -14,6 +14,7 @@ class Equalize : public DataBuffer<float>
 {
 public:
 	Equalize();
+	Equalize(nlohmann::json &config){};
 	Equalize(const Equalize &equalize);
 	Equalize & operator=(const Equalize &equalize);
 	~Equalize();
